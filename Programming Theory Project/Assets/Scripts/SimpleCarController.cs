238abe1497cc7911446f9d03677d6bca8355f86a
JC <jorgeccastro5@gmail.com>
2022-02-03 23:06:16 -0600

    Added some comments and changed two variables into properties in SimpleCarController
    Added comments about OOP Programming pillars.
    
    Variables torquePower and steerAngle were changed to properties (added getters and setters)

--- Programming Theory Project/Assets/Scripts/SimpleCarController.cs
+++ Programming Theory Project/Assets/Scripts/SimpleCarController.cs
@@ -2,52 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// INHERITANCE
+//************** INHERITANCE **************
 public class SimpleCarController : Vehicle
 {
     // Wheel Meshes
-    // Front
     public Transform frontLeftWheelMesh;
     public Transform frontRightWheelMesh;
-    // Rear
     public Transform rearLeftWheelMesh;
     public Transform rearRightWheelMesh;
     // Wheel Colliders
-    // Front
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
-    // Rear
     public WheelCollider wheelRL;
     public WheelCollider wheelRR;
     public float maxTorque = 500f;
     public float brakeTorque = 1000f;
-    // max wheel turn angle;
     public float maxWheelTurnAngle = 30f; // degrees
-                                          // car's center of mass
     public Vector3 centerOfMass = new Vector3(0f, 0f, 0f); // unchanged
     public Vector3 eulertest;
     // PRIVATE
+    //************** ENCAPSULATION **************
     // acceleration increment counter
-    private float torquePower = 0f;
+    private float torquePower { get; set; }
     // turn increment counter
-    private float steerAngle = 30f;
+    private float steerAngle { get; set; }
+
+    private void Awake()
+    {
+        torquePower = 0f;
+        steerAngle = 30f;
+    }
 
     void Start()
     {
         GetComponent<Rigidbody>().centerOfMass = centerOfMass;
     }
-    // Visual updates
+    
     void Update()
     {
+        //************** ABSTRACTION **************
         WheelMovement();
     }
-    // Physics updates
+    
     void FixedUpdate()
     {
+        //************** ABSTRACTION **************
         Move();
     }
 
-    // POLYMORPHISM
+    //************** POLYMORPHISM **************
     public override void Move()
     {
         // CONTROLS - FORWARD & RearWARD
