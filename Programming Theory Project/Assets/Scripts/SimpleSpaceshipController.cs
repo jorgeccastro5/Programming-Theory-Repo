ebe907feeaa415609a72c947949c477c8db7bb1c
JC <jorgeccastro5@gmail.com>
2022-02-04 00:04:58 -0600

    Spaceship racing track scene created.

---
+++ Programming Theory Project/Assets/Scripts/SimpleSpaceshipController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//************** INHERITANCE **************
+public class SimpleSpaceshipController : Vehicle
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //************** POLYMORPHISM **************
+    public override void Move()
+    {
+        // TODO
+    }
+}
