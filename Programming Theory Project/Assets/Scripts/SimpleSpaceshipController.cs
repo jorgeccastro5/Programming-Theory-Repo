b65970b74fbc18b3f5e191f409158e19025afc4a
JC <jorgeccastro5@gmail.com>
2022-02-23 23:42:29 -0600

    Move function changed in Spaceship controller

--- Programming Theory Project/Assets/Scripts/SimpleSpaceshipController.cs
+++ Programming Theory Project/Assets/Scripts/SimpleSpaceshipController.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 //************** INHERITANCE **************
 public class SimpleSpaceshipController : Vehicle
 {
+    public float speed = 10.0f;
+    private Rigidbody shipRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
     }
 
     //************** POLYMORPHISM **************
     public override void Move()
     {
-        // TODO
+        if (Input.GetKey(KeyCode.Space))
+        {
+            shipRb.AddForce(transform.forward * speed);
+        }
+        else
+        {
+            if(shipRb.velocity.magnitude > 0)
+            {
+                shipRb.AddForce(-transform.forward * (speed / 2));
+            }
+
+            if(shipRb.velocity.x < 0 || shipRb.velocity.y < 0 || shipRb.velocity.z < 0)
+            {
+                shipRb.velocity = Vector3.zero;
+            }
+        }
+
+        Debug.Log("Velocity: " + shipRb.velocity);
     }
 }
