d43afe6285d4f0d853f43255dd327aa0324b6cf2
JC <jorgeccastro5@gmail.com>
2022-03-02 23:43:50 -0600

    Version changed and Update in SimpleSpaceshipController

--- Programming Theory Project/Assets/Scripts/SimpleSpaceshipController.cs
+++ Programming Theory Project/Assets/Scripts/SimpleSpaceshipController.cs
@@ -8,32 +8,50 @@ public class SimpleSpaceshipController : Vehicle
     public float horsePower = 25000f;
     private Rigidbody shipRb;
     private float maxSpeed = 300; // kph
+    [SerializeField] private float yAxisOffset = 2f;
+    [SerializeField] private float maxYPosition = 4f;
+    [SerializeField]private float yPositionOriginal = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
         shipRb = GetComponent<Rigidbody>();
+        transform.position = new Vector3(transform.position.x, yPositionOriginal, transform.position.z);
+    }
+
+    void Update()
+    {
+        Move();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Move();
+        Accelerate();
     }
 
     //************** POLYMORPHISM **************
     public override void Move()
     {
+        // TERMINAR
+        float verticalAxis = Input.GetAxis("Vertical");
+        Vector3 newPosition;
+
+        
+    }
+
+    private void Accelerate()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            if(GetSpeed() < maxSpeed)
+            if (GetSpeed() < maxSpeed)
             {
                 shipRb.AddForce(transform.forward * horsePower);
             }
         }
         else
         {
-            if(GetSpeed() > 0)
+            if (GetSpeed() > 0)
             {
                 shipRb.AddForce(-transform.forward * (horsePower / 2));
             }
